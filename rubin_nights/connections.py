be578c000c2869eac99aeb62139f9093a4790265
alserene <118706797+alserene@users.noreply.github.com>
2025-09-02 14:33:56 +1000

    Update get_clients to define token for ConsDbTap

--- rubin_nights/connections.py
+++ rubin_nights/connections.py
@@ -100,6 +100,7 @@ def get_clients(
     if auth_token is not None:
         # Override and use provided token as-is.
         auth = ("user", auth_token)
+        token = auth_token
     else:
         # Set up authentication
         token = get_access_token(tokenfile)
