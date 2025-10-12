ed04479bbcb819d664e087407fd61c4fbc6c2a8a
Lynne Jones <ljones.uw@gmail.com>
2025-10-12 11:41:13 -0700

    Make mypy happy

--- rubin_nights/ts_xml_enums.py
+++ rubin_nights/ts_xml_enums.py
@@ -4,67 +4,79 @@
 
 import enum
 import logging
-from itertools import chain
 
 import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-__all__ = ["ScriptState", "CSCState", "SalIndexExtended", "apply_enum"]
+__all__ = ["ScriptState", "CSCState", "SalIndex", "SalIndexExtended", "apply_enum"]
 
-try:
+# Make mypy happy like this
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
     from lsst.ts.xml.enums.Script import ScriptState
     from lsst.ts.xml.enums.ScriptQueue import SalIndex
     from lsst.ts.xml.sal_enums import State as CSCState
 
-except ModuleNotFoundError:
 
-    logger.warning("Could not import ts-xml; substituting copies but please install lsst-ts-xml.")
-    # Could not import ts_xml so define the enums here
-    # These are fairly stable; but better to import from ts-xml.
+else:
+    try:
+        from lsst.ts.xml.enums.Script import ScriptState
+        from lsst.ts.xml.enums.ScriptQueue import SalIndex
+        from lsst.ts.xml.sal_enums import State as CSCState
 
-    class CSCState(enum.IntEnum):
-        """CSC summaryState constants."""
+    except ModuleNotFoundError:
 
-        OFFLINE = 4
-        STANDBY = 5
-        DISABLED = 1
-        ENABLED = 2
-        FAULT = 3
+        logger.warning("Could not import ts-xml; substituting copies but please install lsst-ts-xml.")
+        # Could not import ts_xml so define the enums here
+        # These are fairly stable; but better to import from ts-xml.
 
-    class ScriptState(enum.IntEnum):
-        """ScriptState constants."""
+        class CSCState(enum.IntEnum):
+            """CSC summaryState constants."""
 
-        UNKNOWN = 0
-        UNCONFIGURED = 1
-        CONFIGURED = 2
-        RUNNING = 3
-        PAUSED = 4
-        ENDING = 5
-        STOPPING = 6
-        FAILING = 7
-        DONE = 8
-        STOPPED = 9
-        FAILED = 10
-        CONFIGURE_FAILED = 11
+            OFFLINE = 4
+            STANDBY = 5
+            DISABLED = 1
+            ENABLED = 2
+            FAULT = 3
 
-    class SalIndex(enum.IntEnum):
-        """Allowed SAL indices for the bin scripts.
+        class ScriptState(enum.IntEnum):
+            """ScriptState constants."""
 
-        The CSC allows other positive values, as well,
-        but those should only be used for unit testing.
-        """
+            UNKNOWN = 0
+            UNCONFIGURED = 1
+            CONFIGURED = 2
+            RUNNING = 3
+            PAUSED = 4
+            ENDING = 5
+            STOPPING = 6
+            FAILING = 7
+            DONE = 8
+            STOPPED = 9
+            FAILED = 10
+            CONFIGURE_FAILED = 11
 
-        MAIN_TEL = 1
-        AUX_TEL = 2
-        OCS = 3
+        class SalIndex(enum.IntEnum):
+            """Allowed SAL indices for the bin scripts.
+
+            The CSC allows other positive values, as well,
+            but those should only be used for unit testing.
+            """
+
+            MAIN_TEL = 1
+            AUX_TEL = 2
+            OCS = 3
 
 
-class ScriptQueueExtensions(enum.IntEnum):
+class SalIndexExtended(enum.IntEnum):
     """Add assigned `salIndex` values for other portions of the
     scriptqueue context feed.
     """
 
+    MAIN_TEL = SalIndex.MAIN_TEL.value
+    AUX_TEL = SalIndex.AUX_TEL.value
+    OCS = SalIndex.OCS.value
     ERRORS = 4
     EXP_SIMONYI = 5
     EXP_AUX = 6
@@ -74,11 +86,6 @@ class ScriptQueueExtensions(enum.IntEnum):
     NARRATIVE_LOG_SIMONYI = 20
     NARRATIVE_LOG_AUX = 21
     NARRATIVE_LOG_OTHER = 22
-
-
-SalIndexExtended = enum.IntEnum(
-    "SalIndexExtended", [(i.name, i.value) for i in chain(SalIndex, ScriptQueueExtensions)]
-)
 
 
 def apply_enum(x: pd.Series, column: str, enumvals: ScriptState | CSCState) -> str:
