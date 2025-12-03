43b4aebba7125b8cc2280ad8809d8b08356dbbfc
Lynne Jones <ljones.uw@gmail.com>
2025-12-03 11:45:43 -0800

    Add base to connection options
    (cherry picked from commit 77b6221d983ba8f233018a95e84b0e808d755c8f)

--- tests/test_connections.py
+++ tests/test_connections.py
@@ -9,36 +9,53 @@ class TestConnections(unittest.TestCase):
     def setUp(self) -> None:
         self.test_dir = Path(__file__).parent
         self.tokenfile = os.path.join(self.test_dir, "data", "dummy_token")
+        # This matches content from dummy_token file
         self.expected_token = "NotARealToken"
+        # This is just for an environment token
+        self.expected_token2 = "NotARealTokenEither"
 
     def test_access_token(self) -> None:
-        # Use the tokenfile
+        # Save current environment settings
+        current_env_token = os.getenv("ACCESS_TOKEN")
+        current_env_tokenfile = os.getenv("ACCESS_TOKEN_FILE")
+        # Replace them with nothing
+        if current_env_token is not None:
+            del os.environ["ACCESS_TOKEN"]
+        if current_env_tokenfile is not None:
+            del os.environ["ACCESS_TOKEN_FILE"]
+
+        # Use the test tokenfile
         token = connections.get_access_token(tokenfile=self.tokenfile)
         self.assertEqual(token, self.expected_token)
-        # Use nothing
-        token = connections.get_access_token()
+        # Use nothing (and use bad default_tokenfile
+        token = connections.get_access_token(default_tokenfile="NoToken")
         if os.getenv("EXTERNAL_INSTANCE_URL") is None:
             self.assertTrue(len(token) == 0)
         # This is expected to get a real token on the RSP
         else:
             self.assertTrue(len(token) > 0)
-        # Use environment variable
-        current_env_val = os.getenv("ACCESS_TOKEN")
-        os.environ["ACCESS_TOKEN"] = self.expected_token
+        # Use environment variable as token
+        os.environ["ACCESS_TOKEN"] = self.expected_token2
+        token = connections.get_access_token()
+        self.assertEqual(token, self.expected_token2)
+        del os.environ["ACCESS_TOKEN"]
+        # Use environment variable for tokenfile
+        os.environ["ACCESS_TOKEN_FILE"] = self.tokenfile
         token = connections.get_access_token()
         self.assertEqual(token, self.expected_token)
-        if current_env_val is not None:
-            os.environ["ACCESS_TOKEN"] = current_env_val
+        del os.environ["ACCESS_TOKEN_FILE"]
+        # Reset pre-environment variables.
+        if current_env_token is not None:
+            os.environ["ACCESS_TOKEN"] = current_env_token
+        if current_env_tokenfile is not None:
+            os.environ["ACCESS_TOKEN_FILE"] = current_env_tokenfile
 
     def test_endpoints(self) -> None:
         # Check definition of some sites
-        site = "usdf"
-        tokenfile = os.path.join(self.test_dir, self.tokenfile)
-        endpoints = connections.get_clients(tokenfile=tokenfile, site=site)
-        self.assertEqual(endpoints["api_base"], "https://usdf-rsp.slac.stanford.edu")
-        site = "usdf-dev"
-        endpoints = connections.get_clients(tokenfile=tokenfile, site=site)
-        self.assertEqual(endpoints["api_base"], "https://usdf-rsp-dev.slac.stanford.edu")
+        for site in ["usdf", "usdf-dev", "summit", "base"]:
+            tokenfile = os.path.join(self.test_dir, self.tokenfile)
+            endpoints = connections.get_clients(tokenfile=tokenfile, site=site)
+            self.assertTrue(isinstance(endpoints["api_base"], str))
         # Check expected clients are added to the dictionary
         clients = ["consdb", "consdb_tap", "efd", "obsenv", "narrative_log", "exposure_log", "night_report"]
         endpoint_keys = list(endpoints.keys())
