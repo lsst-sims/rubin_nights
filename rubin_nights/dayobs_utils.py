1b9e6fdf2705bd0310a45f663eeec2449b20f980
Lynne Jones <ljones.uw@gmail.com>
2026-01-26 15:54:13 -0800

    Add time to dayobs int function

--- rubin_nights/dayobs_utils.py
+++ rubin_nights/dayobs_utils.py
@@ -10,6 +10,7 @@ __all__ = [
     "today_day_obs",
     "yesterday_day_obs",
     "time_to_day_obs",
+    "time_to_day_obs_int",
     "day_obs_str_to_int",
     "day_obs_int_to_str",
     "day_obs_to_date",
@@ -33,6 +34,10 @@ def time_to_day_obs(time: Time) -> str:
     """Return day_obs for astropy Time, formatted as YYYY-MM-DD."""
     return Time(int(time.mjd - 0.5), format="mjd", scale="utc").iso[0:10]
 
+def time_to_day_obs_int(time: Time) -> int:
+    """Return day_obs for astropy Time, integer YYYYMMDD."""
+    day_obs_str = Time(int(time.mjd - 0.5), format="mjd", scale="utc").iso[0:10]
+    return day_obs_str_to_int(day_obs_str)
 
 def day_obs_int_to_str(day_obs: int) -> str:
     """Day_obs integer YYYYMMDD transformed to string YYYY-MM-DD."""
