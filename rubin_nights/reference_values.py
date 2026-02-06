3ce8e7df24ebb234e155413d22de09cc27a48e24
Lynne Jones <ljones.uw@gmail.com>
2026-02-06 10:59:04 -0800

    Update y band zeropoint offset

--- rubin_nights/reference_values.py
+++ rubin_nights/reference_values.py
@@ -24,11 +24,13 @@ SCIENCE_PROGRAMS = ("BLOCK-365", "BLOCK-407", "BLOCK-408", "BLOCK-416", "BLOCK-4
 # Approximate pixel scale
 PLATESCALE = 0.2
 # convert from SIGMA to FWHM
-GAUSSIAN_FWHM_OVER_SIGMA: float = 2.0 * np.sqrt(2.0 * np.log(2.0))
+SIGMA_TO_FWHM: float = 2.0 * np.sqrt(2.0 * np.log(2.0))
+FWHM_TO_SIGMA: float = 1 / SIGMA_TO_FWHM
+
 
 # TODO make these time-dependent
 # current estimate for zeropoint offsets to be able to evaluate clouds=0
-ZEROPOINT_OFFSETS_LSSTCAM = {"u": 0.12, "g": 0.09, "r": 0.15, "i": 0.14, "z": 0.15, "y": 0.13}
+ZEROPOINT_OFFSETS_LSSTCAM = {"u": 0.12, "g": 0.09, "r": 0.13, "i": 0.13, "z": 0.14, "y": 0.02}
 # lsstcomcam offsets based on refcats at the time of processing
 ZEROPOINT_OFFSETS_LSSTCOMCAM = {"u": 0.26, "g": -0.14, "r": -0.09, "i": -0.10, "z": -0.13, "y": -0.18}
 # lsstcomcam offsets for DP1 are probably 0 although might be
